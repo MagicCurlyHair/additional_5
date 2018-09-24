ed6fe97bbd33ce10063ec921ad1e092114642695
MagicCurlyHair <g.shokanov@gmail.com>
2018-09-24 13:15:39 +0300

    function check is now working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketArr = [];
+  let openingBracket;
+  let closingBracket;
+  let lastBracket;
+  for (let i = 0; i < str.length; i++){
+    // iterates over bracketsConfig and gets current opening and closing brackets
+    for (let j = 0; j < bracketsConfig.length; j++){
+      openingBracket = bracketsConfig[j][0];
+      closingBracket = bracketsConfig[j][1];
+      // if current bracket is an opening bracket
+      if (str[i] == openingBracket){
+        /* checks if openingBracket is equal to closing bracket and if
+        it is the last item of bracketArr; if it is - pops it*/
+        if (openingBracket == closingBracket && bracketArr[bracketArr.length - 1] == openingBracket){
+            bracketArr.pop();
+          }
+        // if previous test is failed - pushes opening bracket to bracketArr
+        else {
+          bracketArr.push(openingBracket);
+        }
+      }
+      // checks if current bracket is a closing bracket
+      else if (str[i] == closingBracket){
+        // pops last bracket from array and checks if it was a proper opening bracket
+        lastBracket = bracketArr.pop();
+        if (lastBracket != openingBracket){
+          return false;
+        }
+      }
+    }
+  }
+  // returns false if there are unclosed brackets in array
+  if (bracketArr.length != 0){
+    return false;
+  }
+  return true;
 }
